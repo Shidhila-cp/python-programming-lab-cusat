7c0b3c532e9274f1fa70672ce1e02227bf83a135
Abdul Hakkeem P A <abdulhakkeempa2002@gmail.com>
2022-04-10 17:22:26 +0530

    pca updated

--- Python - Lab Cycles/Python - Lab Cycle 3/Question - 3/lab_cycle_3_q3.py
+++ Python - Lab Cycles/Python - Lab Cycle 3/Question - 3/lab_cycle_3_q3.py
@@ -16,6 +16,8 @@ Original file is located at
 # Commented out IPython magic to ensure Python compatibility.
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 # %matplotlib inline
 
 #reading the csv file into df.
@@ -25,6 +27,33 @@ df = pd.read_csv ('Iris.csv')
 df.Species.value_counts().plot(figsize=(8,6),kind='bar',color=['r','g','b'],xlabel='Species',ylabel='Frequency of Species')
 plt.title("Frequency Bar Graph")
 plt.show()
+
+print("\nPCA Graph")
+#plotting the principal analysis graph for two components
+X = df.iloc[:, 1:5].values
+X_std = StandardScaler().fit_transform(X)
+pca = PCA(n_components=2)
+principalComponents = pca.fit_transform(X_std)
+principalDf = pd.DataFrame(data = principalComponents
+             , columns = ['principal component 1', 'principal component 2'])
+finalDf = pd.concat([principalDf, df['Species']], axis = 1)
+
+fig = plt.figure(figsize = (10,8))
+ax = fig.add_subplot(1,1,1)
+ax.set_xlabel('First Principle Component')
+ax.set_ylabel('Second Principal Component')
+ax.set_title('PCA Graph')
+targets = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
+colors = ['r', 'g', 'b']
+for target, color in zip(targets,colors):
+    indicesToKeep = finalDf['Species'] == target
+    ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1']
+               , finalDf.loc[indicesToKeep, 'principal component 2']
+               , c = color
+               , s = 50)
+ax.legend(targets)
+plt.show()
+
 
 print("\nDistribution Histogram\n")
 
@@ -37,6 +66,7 @@ plt.xlabel("Sepal Length cm")
 plt.ylabel("Distribution Count")
 plt.show()
 
+print()
 #histogram for sepal width.
 plt.figure(figsize = (7, 5))
 x = df.SepalWidthCm
@@ -46,6 +76,7 @@ plt.xlabel("Sepal Width cm")
 plt.ylabel("Distribution Count")
 plt.show()
 
+print()
 #histogram for petal length.
 plt.figure(figsize = (7, 5))
 x = df.PetalLengthCm
@@ -55,7 +86,8 @@ plt.xlabel("Petal Length cm")
 plt.ylabel("Distribution Count")
 plt.show()
 
-#histogram for petal width.
+print()
+#histogram for petal widht.
 plt.figure(figsize = (7, 5))
 x = df.PetalWidthCm
 plt.hist(x, color = "orange")
